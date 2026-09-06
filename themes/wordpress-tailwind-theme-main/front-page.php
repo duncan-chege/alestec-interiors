81ca2dc69aecdf3220d3c046564d1357428f2442
duncan-chege <dshege4@gmail.com>
2026-09-06 07:53:12 +0300

    resize the second set of images in the homepage

--- themes/wordpress-tailwind-theme-main/front-page.php
+++ themes/wordpress-tailwind-theme-main/front-page.php
@@ -65,8 +65,8 @@
                 <span class="text-xs md:text-sm tracking-wide leading-tight mt-1">YEARS OF<br>EXPERIENCE</span>
             </div>
 
-            <div class="absolute top-3/4 -translate-y-1/2 animate-slide-left-centered reveal">
-                     <img class="w-full h-full object-cover" src="<?php echo get_template_directory_uri();?>/assets/images/welcome-inner-img.png" alt="interior showcase">
+            <div class="absolute top-[80%] -translate-y-1/2 animate-slide-left-centered reveal">
+                     <img class="w-2/3 lg:w-full lg:h-full object-cover" src="<?php echo get_template_directory_uri();?>/assets/images/welcome-inner-img.png" alt="interior showcase">
             </div>
         </div>
     </div>
